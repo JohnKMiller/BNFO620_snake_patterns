00516212f72b47dd32e403fab225730489f3efe9
John <millerjk3@vcu.edu>
2024-02-28 01:14:19 -0500

    Update creating function to query database

--- snake_api_collab.R
+++ snake_api_collab.R
@@ -1,3 +1,27 @@
-#Get list of snake species
-#Call database and parse info
-#Format download and print
+install.packages("spocc")
+library(spocc)
+
+#function to Call database and parse info of name and image URL into csv
+getPhotosINat <- function(snakeName,inatLimit = 100){
+  #makes a df
+  snakes.df <-data.frame()
+  #query database
+  inat.df <- occ(query = snakeName, from = "inat", limit = inatLimit, inatopts = list(quality = "research"),throw_warnings = FALSE)
+  #find photos
+  photoData.df <- inat.df$inat$data[[1]]$photos
+  #collect all photos
+  for (y in 1:length(photoData.df)){
+      #get the values 
+      New_row = list(toString(inat.df$inat$data[[1]]$name[[y]]),photoData.df[[y]]['url'],photoData.df[[y]]['id'])
+      #append to df
+      snakes.df <- rbind(snakePics,New_row)
+  }
+  #update column names
+  colnames(snakes.df) <- c('snake_name','url','id')
+  return(snakes.df)
+}
+
+#testing tools
+
+snake_photos <- getPhotosINat('Lampropeltis californiae')
+
