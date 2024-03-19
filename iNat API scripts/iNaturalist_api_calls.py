d53dcf917a260b12bdffd8e82d6c1d846e4cefd7
KyleStoecker <98105706+KyleStoecker@users.noreply.github.com>
2024-03-19 01:14:04 -0400

    Update iNaturalist_api_calls.py

--- iNat API scripts/iNaturalist_api_calls.py
+++ iNat API scripts/iNaturalist_api_calls.py
@@ -1,45 +1,47 @@
 import requests 
 import pandas as pd
+import time
+# Take input of file name and desired output type
+#import file of snake names and form them into a query statement
 
-#take input of file name and desired output type 
+# Take input of file name and desired output type
+snake_data = pd.read_csv("inputfile.csv") # assuming your file is in CSV format
+snake_names = snake_data.Species
+format_names = []
 
+# Initialize a list to store tuples of species name and photo URL
+species_photos = []
 
-#"import file of snake names and form them into a query statement
-snake_data = pd.read_csv("tempfile.csv")
-snake_names = snake_data.species
-format_names = ""
-
-# going through the csv and formatting them into a string to make an api call
+# Going through the CSV and formatting them into a string to make an API call
 for i in snake_names:
     format_names.append(i + '&')
-# remove & at end of string
-format_names_no_end = format_names[0: (len(format_names)-1)]
-# make query for taxon names
+
+format_names_no_end = ''.join(format_names[:-1])  # Using join to concatenate list of strings
+
+# Make query for taxon names
 query = "observations?taxon_name=%s" % (format_names_no_end)
 
-test_query = "observations?taxon_name=Lampropeltis-californiae"
-# call api
 endpoint = "https://api.inaturalist.org/v1/"
-response = requests.get(endpoint + test_query)
-data = response.json()
 
-# make list of photos
-photo_list = []
-photo_dict= {}
+for name in snake_names: # Loop through each snake name
+    current_query = "observations?taxon_name=" + name
+    response = requests.get(endpoint + current_query)
+    data = response.json()
 
-x = 0
-while x < len(data['results']):
-    pic = data['results'][x]['photos']
-    photo_list.append(pic[0]['url'])
-    photo_dict.update()
-    x = x+1
-print(len(photo_list))
-print(len(data['results']))
-# put photo urls and names into dataframe
-photo_data = pd.DataFrame(photo_list)
-photo_data.to_excel('temp_output.xlsx', header=False, index=False)
+    if 'results' in data: # Check if there are any results
+        for result in data['results']:
+            if 'photos' in result and result['photos']: # Check if photos exist for the current result
+                species_photos.append((name, result['photos'][0]['url'])) # Append the tuple (species name, photo URL) to the list
 
-# export to desired format
+    time.sleep(2) # Introduce a delay of 2 seconds between API calls
+
+# Convert the list of tuples to a DataFrame
+photo_data = pd.DataFrame(species_photos, columns=['Species', 'Photo URL'])
+
+# Export to desired format
+#photo_data.to_csv('exportfile', header=False, index=False)
+
+
 
 #&Lampropeltis-elapsoides
 
